1f2308fe04bcc4d01bd13509f9ed84c7fdbbea59
Jithin Zacharia <jithin.mscis3@iiitmk.ac.in>
2020-02-24 19:28:06 +0530

    Deleting project

---
+++ demo/src/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import createHistory from 'history/createBrowserHistory';
+
+export const history = createHistory();
